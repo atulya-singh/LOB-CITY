7eed57f077b802b2c1ae9720136532505908d1e2
Atulya Singh <atulyarajsingh@gmail.com>
2026-03-14 01:01:18 -0400

    Made changes to include the Pre Trade Risk checks

--- OrderEntryGateway.h
+++ OrderEntryGateway.h
@@ -3,14 +3,16 @@
 #include "MemoryPool.h"
 #include "OrderBook.h"
 #include "FixParser.h"
+#include "Riskengine.h"
 
 class OrderEntryGateway {
 private:
     OrderPool* pool;
     OrderBook* book;
+    RiskEngine risk; 
 
 public:
-    OrderEntryGateway(OrderPool* p, OrderBook* b) : pool(p), book(b) {}
+    OrderEntryGateway(OrderPool* p, OrderBook* b, const RiskConfig& cfg = RiskConfig()) : pool(p), book(b), risk(cfg) {}
 
     inline void onParsedMessage(const ParsedFixMessage& msg) {
         if (msg.msgType == 'D') { // New Order Single
@@ -19,9 +21,25 @@ public:
             handleCancelRequest(msg);
         }
     }
+    RiskEngine& getRiskEngine(){return risk;}
+    const RiskEngine& getRiskEngine() const {return risk;}
 
 private:
     inline void handleNewOrderSingle(const ParsedFixMessage& msg) {
+        //STEP 0: First check perform the PRE-TRADE RISK CHECK
+        //happens before we touch memory pool
+
+        risk.updateBBO(book->getBestBid(), book->getBestAsk());
+        RiskRejectReason rejection = risk.checkOrder(msg);
+        if(rejection != RiskRejectReason::NONE){
+#ifdef ENABLE_RISK_LOGGING
+            std::cout << "[RISK] REJECTED Order " << msg.clOrdID 
+                      << " | Reason: " << rejectReasonToString(rejection) << "\n";
+#endif
+            return; // <-- Order dies here. Never reaches the book.
+        }
+        
+        
         // 1. Convert string_view clOrdID back to a fast int (uint64_t)
         // Since you already have parseFastInt in FixParser.h, we can reuse it!
         OrderId numericId = static_cast<OrderId>(
