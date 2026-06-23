2a34aff542e7cd965eb04c218ec1af5b648355ea
Atulya Singh <atulyarajsingh@gmail.com>
2026-06-23 23:26:32 +0530

    Added push method for Ring Buffer

--- src/marketdata/RingBufferBBO.h
+++ src/marketdata/RingBufferBBO.h
@@ -7,4 +7,28 @@
 class RingBufferBBO {
 private:
     BboMessage buffer[SIZE];
+    // head and tail set to 0 at the start
+    std::atomic<size_t> head{0};
+    std::atomic<size_t> tail{0}; 
+
+public:
+
+    inline bool push(const BboMessage& msg){
+        
+        size_t current_tail = tail.load(std::memory_order_acquire);
+        size_t current_head = head.load(std::memory_order_relaxed);
+
+        size_t new_head = (current_head + 1) % SIZE;
+        
+        // Buffer is full 
+        if (new_head == current_tail){
+            return false;
+        }
+
+        buffer[current_head] = msg;
+
+       head.store(new_head, std::memory_order_release);
+       return true;
+
+    }
 };
