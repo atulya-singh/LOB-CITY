d77df97a532349da141968de88c174e63c1dff7f
Atulya Singh <atulyarajsingh@gmail.com>
2026-06-26 20:08:03 +0530

    Minor changes

--- src/core/OrderBook.cpp
+++ src/core/OrderBook.cpp
@@ -17,7 +17,7 @@ void OrderBook::recordTrade(OrderId buyId, OrderId sellId, Price price, Quantity
     timespec ts;
     clock_gettime(CLOCK_REALTIME_COARSE, &ts);
     uint64_t nanoseconds = (ts.tv_sec * 1000000000LL) + ts.tv_nsec;
-        tradeLog.push_back({buyId, sellId, price, qty, nanoseconds});
+    tradeLog.push_back({buyId, sellId, price, qty, nanoseconds});
 }
 
 void OrderBook::matchBuyOrder(Order* buyOrder) {
